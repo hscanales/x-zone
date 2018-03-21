0b23771a900b461de0e5c02480bbc226ee4cf311
Hector Canales <00136317@uca.edu.sv>
2018-03-21 17:22:59 -0600

    Creation

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ * 
+ * 
+ */
+package main;
+
+/**
+ *
+ * @author HsCanales <00136317@uca.edu.sv>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
