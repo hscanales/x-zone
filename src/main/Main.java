4ecb354a221b279ad0d6a7b50370f5c7cfdaec3d
Hector Canales <00136317@uca.edu.sv>
2018-07-06 19:36:59 -0600

    fix main

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,9 +19,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //JFrame login = new Login();
-        //login.setVisible(true);
+        
+        JFrame login = new Login();
+        login.setVisible(true);
         
     }
     
