2149e6a229204541d0613906bddd55015dab7b55
Hector Canales <00136317@uca.edu.sv>
2018-07-08 20:28:04 -0600

    Fix Bugs

--- src/GUI/GO3.java
+++ src/GUI/GO3.java
@@ -55,8 +55,9 @@ public class GO3 extends JFrame {
         this.setLocationRelativeTo(null);
         Formulario();
         JFrame frame = new JFrame();
-        JOptionPane.showMessageDialog(frame, "Final BOSS $6.99", "DLC",
-            JOptionPane.INFORMATION_MESSAGE);
+        
+        
+        
         addKeyListener(new TAdapter());
         Container container = getContentPane();
         container.add(personaje);
@@ -66,6 +67,9 @@ public class GO3 extends JFrame {
         mapa = MapReader.reader("src/GUI/Mapa/matriz3.txt");
         emov1 = new EnemigoMov(enemigo1, personaje, 384, 480, 160, 320, Xander);
         emov1.start();
+        JOptionPane.showMessageDialog(frame, "Final BOSS $6.99  \n https://www.paypal.me/hectorcanalessv \n", "DLC",
+            JOptionPane.INFORMATION_MESSAGE);
+        
     }
     
     public void Formulario(){
